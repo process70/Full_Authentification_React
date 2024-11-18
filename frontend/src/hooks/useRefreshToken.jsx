1fd2d749583ceba9d7daacaa04d7f14043847842
process70 <163150253+process70@users.noreply.github.com>
2024-11-18 19:36:15 +0000

    Update useRefreshToken.jsx

--- frontend/src/hooks/useRefreshToken.jsx
+++ frontend/src/hooks/useRefreshToken.jsx
@@ -8,8 +8,6 @@ const useRefreshToken = () => {
         // withCredentials: true allow to send the secure cookie
         const response = await axios.get('/refresh', {withCredentials: true})
         setAuth(prev => {
-            console.log('previous auth: '+prev.accessToken)
-            console.log('the new access token: '+response?.data?.accessToken)
             /* set the accessToken in auth context with the new one
             set also the roles in case where the user refresh the page in this case we lost roles */
             return { ...prev, accessToken: response?.data?.accessToken, roles: response?.data?.roles}
