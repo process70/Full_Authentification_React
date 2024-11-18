7868f4564a1870ada1fdd09251939ddc8e5c05e0
process70 <163150253+process70@users.noreply.github.com>
2024-11-18 14:24:12 +0000

    Update allowedOrigins.js

--- backend/config/allowedOrigins.js
+++ backend/config/allowedOrigins.js
@@ -1,7 +1,8 @@
 const allowedOrigins = [
     'http://localhost:3000',
     'https://www.dandrepairshop.com',
-    'https://dandrepairshop.com'
+    'https://dandrepairshop.com',
+    'https://full-authentification-react.onrender.com'
 ]
 
 module.exports = allowedOrigins
