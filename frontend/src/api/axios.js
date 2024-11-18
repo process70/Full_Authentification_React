93fe683d8a88d999e304f9f87433a04f4b6337e0
process70 <163150253+process70@users.noreply.github.com>
2024-11-18 14:16:10 +0000

    Update axios.js

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const BASE_URL = 'http://localhost:5000/auth';
+const BASE_URL = 'https://full-authentification-react.onrender.com/auth';
 
 export default axios.create({
     baseURL: BASE_URL
