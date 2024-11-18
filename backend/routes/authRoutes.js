4b5d7e217641c0c37050c1379211f1141cd03a35
process70 <163150253+process70@users.noreply.github.com>
2024-11-18 18:18:41 +0000

    Update authRoutes.js

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,11 +1,12 @@
 const express = require("express")
 const authRoutes = express.Router()
 const loginLimiter = require("../middleware/loginLimiter")
-const { login, logout, refresh, register } = require("../controllers/authController")
+const { login, logout, refresh, register, getAllUsers } = require("../controllers/authController")
 
 authRoutes.get("/refresh", refresh)
 authRoutes.post("/register", register)
 authRoutes.post("/login", loginLimiter, login)
 authRoutes.post("/logout", logout)
+authRoutes.get("/users", getAllUsers)
 
 module.exports = authRoutes
